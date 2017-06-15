518d567a39b8faa5d43d54268df6bde1559a96de
zezeon <zezeon@msn.com>
2017-06-16 00:38:39 +0900

    616-

--- algo.java
+++ algo.java
@@ -1,16 +1,60 @@
 import java.io.*;
 import javax.swing.*;
 import java.util.*;
+import java.lang.Thread.*;
 
-enum Members {Bob, Jerry, Cole};
+class Buffer {
+	private int data;
+	private boolean empty = true;
+	public synchronized int get() {
+		while(empty) {
+			try {
+				wait();
+			} catch(InterruptedException e) {}
+		}
+		empty = true;
+		notifyAll();
+		return data;
+	}
+	public synchronized void put(int n) {
+		while(!empty) {
+			try {
+				wait();
+			} catch(InterruptedException e) {}
+		}
+		data = n;
+		empty = false;
+		notifyAll();
+	}
+}
+
+class MyThread implements Runnable {
+	private Buffer buffer;
+	public MyThread(Buffer b) {
+		buffer = b;
+	}
+	public void run() {
+		for(int i=0; i<10; i++) System.out.println(buffer.get()); 
+	}
+}
+
+class MTh extends Thread {
+	private Buffer buffer;
+	public MTh(Buffer b) {
+		buffer = b;
+	}
+	public void run() {
+		for(int i=0; i<10; i++) buffer.put(i);
+	}
+}
+
 public class algo {
 	static public void main(String[] args) {
-		Members m = Members.Bob;
-		for(Members me : Members.values()) {
-			System.out.println(me);
-			System.out.println(m.ordinal());
-			System.out.println(Members.valueOf("Bob"));
-		}
+		Buffer b = new Buffer();
+		Thread mt = new Thread(new MyThread(b));
+		MTh m = new MTh(b);
+		mt.start();
+		m.start();
 	}
 }
 
---
+++ collection.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class collection {
+	public static void main(String[] av) {
+		Set<String> s = new HashSet();
+		s.add("fda");
+		s.add("fsjal");
+		System.out.println(s);
+	}
+}
